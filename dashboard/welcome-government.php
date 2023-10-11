312081d6644b6c2ee0b98f6423d0314ade058a47
Jordan Prime <jordanprime@me.com>
2023-10-11 11:10:23 +1030

    rvrdfavfdvafvafdv

--- dashboard/welcome-government.php
+++ dashboard/welcome-government.php
@@ -56,9 +56,16 @@ else if ($_SESSION['user_type'] != 'government') {
                             $studentage = date_diff(date_create(date("Y-m-d")), date_create($_SESSION['student']['dob']));
                             $studentage = $studentage->format('%y');
                             $studentnumber = $_SESSION['student']['contact_number'];
-                            echo "<a href='../search/selectuser.php?username=-1&usertype=learner'>Clear Student</a> ";
-                            echo "<a href='../search/search.php?usertype=student'>Change Student</a> ";
-                            echo '<a href="../login/learner-login.php">Create New Student</a><br>';
+                            echo "<a href='../search/selectuser.php?username=-1&usertype=learner'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x' viewBox='0 0 16 16'>
+                            <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>
+                          </svg> Clear Student</a> ";
+                            echo "<a href='../search/search.php?usertype=student'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'>
+                            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'/>
+                            </svg> Change Student</a> ";
+                            echo "<a href='../login/learner-login.php'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-add' viewBox='0 0 16 16'>
+                            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'/>
+                            <path d='M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z'/>
+                          </svg> Create New Student</a><br>";
                             echo "<p>Name: {$studentname}</p>
                             <ul>
                                 <li>Drivers License: {$studentlicense}</li>
@@ -72,8 +79,13 @@ else if ($_SESSION['user_type'] != 'government') {
                         } else {
                             $studentname = "No Student Selected";
                             echo "<p>Student Name: {$studentname}</p>";
-                            echo '<a href="../search/search.php?usertype=student">Search Student</a><br>';
-                            echo '<a href="../login/learner-login.php">Create New Student</a><br>';
+                            echo "<a href='../search/search.php?usertype=student'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'>
+                            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'/>
+                            </svg> Search Student</a><br>";
+                            echo "<a href='../login/learner-login.php'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-add' viewBox='0 0 16 16'>
+                            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'/>
+                            <path d='M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z'/>
+                          </svg> Create New Student</a><br>";
                         }
                     ?>
                 </div>
@@ -88,8 +100,16 @@ else if ($_SESSION['user_type'] != 'government') {
                             $instructorage = date_diff(date_create(date("Y-m-d")), date_create($_SESSION["instructor"]["dob"]));
                             $instructorage = $instructorage->format('%y');
                             $instructornumber = $_SESSION["instructor"]["contact_number"];
-                            echo "<a href='../search/selectuser.php?username=-1&usertype=instructor'>Clear Instructor</a> ";
-                            echo '<a href="../search/search.php?usertype=instructor">Change Instructor</a><br>';
+                            echo "<a href='../search/selectuser.php?username=-1&usertype=instructor'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x' viewBox='0 0 16 16'>
+                            <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>
+                          </svg> Clear Instructor</a> ";
+                            echo "<a href='../search/search.php?usertype=instructor'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'>
+                            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'/>
+                            </svg> Change Instructor</a> ";
+                            echo "<a href='../login/instructor-login.php'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-add' viewBox='0 0 16 16'>
+                            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'/>
+                            <path d='M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z'/>
+                          </svg> Create New Instructor</a> ";
                             echo "<p>Name: {$instructorname}</p>
                             <ul>
                                 <li>Drivers License: {$instructorlicense}</li>
@@ -97,14 +117,18 @@ else if ($_SESSION['user_type'] != 'government') {
                                 <li>Date of Birth: {$instructordob} ( Age: {$instructorage} )</li>
                                 <li>Contact Number: {$instructornumber}</li>
                             </ul>
-                            <a href='../search/search.php?usertype=instructor'>Change Instructor</a><br>
                             <a href='../instructors/report.php'>View Report</a>
                             ";
                         } else {
                             $instructorname = "No Instructor Selected";
                             echo "<p>Instructor Name: {$instructorname}</p>";
-                            echo '<a href="../search/search.php?usertype=instructor">Search Instructor</a><br>';
-                            echo '<a href="../login/instructor-login.php">Create New Instructor</a><br>';
+                            echo "<a href='../search/search.php?usertype=instructor'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'>
+                            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'/>
+                            </svg> Search Instructor</a><br>";
+                            echo "<a href='../login/instructor-login.php'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-add' viewBox='0 0 16 16'>
+                            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'/>
+                            <path d='M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z'/>
+                          </svg> Create New Instructor</a><br>";
                         }
                     ?>
                 </div>
@@ -119,8 +143,16 @@ else if ($_SESSION['user_type'] != 'government') {
                             $qsdage = date_diff(date_create(date("Y-m-d")), date_create($_SESSION["qsd"]["dob"]));
                             $qsdage = $qsdage->format('%y');
                             $qsdnumber = $_SESSION["qsd"]["contact_number"];
-                            echo "<a href='../search/selectuser.php?username=-1&usertype=qsd'>Clear QSD</a> ";
-                            echo "<a href='../search/search.php?usertype=qsd'>Change QSD</a> ";
+                            echo "<a href='../search/selectuser.php?username=-1&usertype=qsd'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x' viewBox='0 0 16 16'>
+                            <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>
+                          </svg> Clear QSD</a> ";
+                            echo "<a href='../search/search.php?usertype=qsd'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'>
+                            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'/>
+                            </svg> Change QSD</a> ";
+                            echo "<a href='../login/qsd-login.php'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-add' viewBox='0 0 16 16'>
+                            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'/>
+                            <path d='M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z'/>
+                          </svg> Create New QSD</a><br>";
                             echo "<p>Name: {$qsdname}</p>
                             <ul>
                                 <li>Drivers License: {$qsdlicense}</li>
@@ -128,13 +160,17 @@ else if ($_SESSION['user_type'] != 'government') {
                                 <li>Date of Birth: {$qsddob} ( Age: {$qsdage} )</li>
                                 <li>Contact Number: {$qsdnumber}</li>
                             </ul>
-                            <a href='../login/qsd-login.php'>Create New QSD</a><br>
                             ";
                         } else {
                             $qsdname = "No QSD Selected";
                             echo "<p>Qualified Supervising Driver: {$qsdname}</p>";
-                            echo '<a href="../search/search.php?usertype=qsd">Search Qualified Supervising Driver</a><br>';
-                            echo '<a href="../login/qsd-login.php">Create New QSD</a><br>';
+                            echo "<a href='../search/search.php?usertype=qsd'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'>
+                            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'/>
+                            </svg> Search QSD</a><br>";
+                            echo "<a href='../login/qsd-login.php'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-add' viewBox='0 0 16 16'>
+                            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'/>
+                            <path d='M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z'/>
+                          </svg> Create New QSD</a><br>";
                         }
                     ?>
                 </div>
--- dashboard/welcome-instructor.php
+++ dashboard/welcome-instructor.php
@@ -78,22 +78,38 @@ $result = mysqli_stmt_get_result($statement);
                             $studentdob = $_SESSION['student']['dob'];
                             $studentage = date_diff(date_create(date("Y-m-d")), date_create($studentdob));
                             $studentage = $studentage->format('%y');
-                            echo "<a href='../search/selectuser.php?username=-1&usertype=learner'>Clear Student</a> ";
-                            echo "<a href='../search/search.php?usertype=student'>Change Student</a> ";
-                            echo "<a href='../login/learner-login.php'>Create New Student</a>";
+                            echo "<a href='../search/selectuser.php?username=-1&usertype=learner'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x' viewBox='0 0 16 16'>
+                            <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>
+                          </svg> Clear Student</a> ";
+                            echo "<a href='../search/search.php?usertype=student'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'>
+                            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'/>
+                            </svg> Change Student</a> ";
+                            echo "<a href='../login/learner-login.php'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-add' viewBox='0 0 16 16'>
+                            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'/>
+                            <path d='M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z'/>
+                          </svg> Create New Student</a>";
                             echo "<p>Student Name: {$studentname}</p>";
                             echo "<ul>
                                 <li>Address: {$studentaddress}</li>
                                 <li>Age: {$studentage}</li>
                                 <li>Contact Number: {$studentnumber}</li>
                             </ul>";
-                            echo '<a href="../logbooks/logbook-entry.php">Add a new logbook entry</a><br>';
-                            echo('<a href="../instructors/issue-bill.php">Issue Bill</a><br><br>');
+                            echo '<a href="../logbooks/logbook-entry.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                          </svg> Add a new logbook entry</a><br>';
+                            echo '<a href="../instructors/issue-bill.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                            <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                          </svg> Issue Bill</a><br><br>';
                         } else {
                             $studentname = "No Student Selected";
                             echo "<p>Student Name: {$studentname}</p>";
-                            echo "<a href='../search/search.php?usertype=student'>Search Student</a> ";
-                            echo "<a href='../login/learner-login.php'>Create New Student</a>";
+                            echo "<a href='../search/search.php?usertype=student'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'>
+                            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'/>
+                            </svg> Search Student</a><br>";
+                            echo "<a href='../login/learner-login.php'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-add' viewBox='0 0 16 16'>
+                            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'/>
+                            <path d='M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z'/>
+                          </svg> Create New Student</a>";
                         }
                     ?>
                 </div>
